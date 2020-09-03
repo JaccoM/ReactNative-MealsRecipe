eedc0320c17c5f3d3b465c1d961191fc435b5336
Jacco Mulders <jacco.mulders@weareyou.com>
2020-09-03 17:23:51 +0200

    Progress

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,23 +8,19 @@ import {
 } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
     const renderGridItem = (itemData) => {
-        return (
-            <TouchableOpacity
-                style={styles.gridItem}
-                onPress={() => {
-                    props.navigation.navigate('CategoryMeals', {
-                        categoryId: itemData.item.id,
-                    })
-                }}
-            >
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
-        );
+        return <CategoryGridTile
+                    title={itemData.item.title}
+                    color={itemData.item.color}
+                    onSelect={() => {
+                        props.navigation.navigate('CategoryMeals', {
+                            categoryId: itemData.item.id,
+                        })
+                    }}
+                />;
     };
 
     return (
@@ -43,11 +39,6 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150,
-    }
 });
 
 export default CategoriesScreen;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,31 +3,44 @@ import {
     View,
     Text,
     StyleSheet,
-    Button,
+    FlatList,
 } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
-const CategoryMealsScreen = props => {
+import MealItem from '../components/MealItem';
+
+const CategoryMealScreen = props => {
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+    const displayedMeals = MEALS.filter(
+        meal => meal.categoryIds.indexOf(catId) >= 0
+    );
+
+    const renderMealItem = itemData => {
+        return <MealItem
+                    title={itemData.item.title}
+                    duration={itemData.item.duration}
+                    complexity={itemData.item.complexity}
+                    affordability={itemData.item.affordability}
+                    image={itemData.item.imageUrl}
+                    onSelectMeal={() => {}}
+                />
+    };
 
     return (
         <View style={styles.screen}>
-            <Text>Category Meals Screen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Go to Details" onPress={() => {
-                props.navigation.navigate('MealDetail');
-            }} />
-            <Button title="Go Back" onPress={() => {
-                props.navigation.goBack();
-            }} />
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+                style={{width: '100%'}}
+            />
         </View>
     );
 };
 
-CategoryMealsScreen.navigationOptions = (navigationData) => {
+CategoryMealScreen.navigationOptions = (navigationData) => {
     const catId = navigationData.navigation.getParam('categoryId');
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
@@ -44,4 +57,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
